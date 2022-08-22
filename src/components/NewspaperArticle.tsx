3b90dc82e2f7ee8a94d79b16211acde605ec98c2
Cristian Ojeda <cristianojeda.maimo@gmail.com>
2022-08-22 14:50:22 -0300

    newspaper modified

--- src/components/NewspaperArticle.tsx
+++ src/components/NewspaperArticle.tsx
@@ -14,18 +14,17 @@ export const NewspaperArticle: React.FC<Props> = ({ title, image, description, f
   return (
     <Box >
         <Box position='relative' backgroundColor='white'>
+            <Box color='black' padding='1rem' className='paper' >
+                <Stack borderLeft='2px dashed #333' spacing={0} paddingLeft='1rem'>
 
-        <Box color='black' padding='1rem' className='paper' >
-            <Stack borderLeft='2px dashed #333' spacing={0} paddingLeft='1rem'>
-
-            <Heading as='h2' size='lg' paddingBottom='1rem'>{ parse(title) }</Heading>
-            <AspectRatio ratio={4/3} maxWidth='400px'>
-                <Image src={ image } filter='grayscale(100%)' _hover= {{ filter: 'unset' }} />
-            </AspectRatio>
-                <Box backgroundColor='gray.800' color='white' fontSize='sm' marginTop={0}>{ fecha } - { epigraph} </Box>
-            <Text padding='0.5rem' >{ parse(description) }</Text>
-            </Stack>
-        </Box>
+                <Heading as='h2' size='lg' paddingBottom='1rem'>{ parse(title) }</Heading>
+                <AspectRatio ratio={4/3} maxWidth='400px'>
+                    <Image src={ image } filter='grayscale(100%)' _hover= {{ filter: 'unset' }} />
+                </AspectRatio>
+                    <Box backgroundColor='gray.800' color='white' fontSize='sm' marginTop={0}>{ fecha } - { epigraph} </Box>
+                <Text padding='0.5rem' >{ parse(description) }</Text>
+                </Stack>
+            </Box>
         </Box>
     </Box>
   )
