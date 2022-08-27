4624b2eec72a4e4e3859a511ca5fb76e4353a08f
Cristian Ojeda <cristianojeda.maimo@gmail.com>
2022-08-27 20:48:51 -0300

    social icons agregados

--- src/SearchScreen.tsx
+++ src/SearchScreen.tsx
@@ -1,4 +1,4 @@
-import { Box, SimpleGrid, GridItem, Image, Text, Container, Heading, Link } from '@chakra-ui/react';
+import { Box, SimpleGrid, GridItem, Image, Text, Container, Heading, Link, Button } from '@chakra-ui/react';
 import React, { useState } from 'react'
 import { useParams } from 'react-router-dom'
 import { useEffect } from 'react';
@@ -6,6 +6,7 @@ import api from './services/api';
 import { GSearchRes } from './interfaces/interfaces';
 import Post from './components/Post';
 import { NewspaperArticle } from './components/NewspaperArticle';
+import { EmailIcon, FacebookIcon, TwitterIcon, WhatsappIcon } from './components/icons';
 
 export const SearchScreen:React.FC = () => {
     const { id = '' } = useParams()
@@ -97,15 +98,20 @@ export const SearchScreen:React.FC = () => {
             }
               </Box>
             </Box>
-            <Box display='flex' justifyContent='space-around' alignItems='center' padding='1em'>
-              <Box>
-              <Link className="resp-sharing-button__link" 
-              href="https://facebook.com/sharer/sharer.php?u=www.parendematarnos.com.ar" 
-              target="_blank" rel="noopener" aria-label="Share on Facebook">
-        <div className="resp-sharing-button resp-sharing-button--facebook resp-sharing-button--large"><div aria-hidden="true" className="resp-sharing-button__icon resp-sharing-button__icon--solid">
-          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M18.77 7.46H14.5v-1.9c0-.9.6-1.1 1-1.1h3V.5h-4.33C10.24.5 9.5 3.44 9.5 5.32v2.15h-3v4h3v12h5v-12h3.85l.42-4z"/></svg>
-          </div>Compartir en Facebook</div>
-      </Link>
+            <Box display='flex' justifyContent='space-around' padding='1em'>
+              <Box className='buttons' display='flex' flexDirection='column' justifyContent={'space-around'}>
+                <Button colorScheme='facebook' leftIcon={ <FacebookIcon/> } >
+                  Facebook
+                </Button>
+                <Button colorScheme='twitter' leftIcon={ <TwitterIcon/> } >
+                  Twitter
+                </Button>
+                <Button colorScheme='teal' leftIcon={<EmailIcon/>} >
+                  Email
+                </Button>
+                <Button colorScheme='whatsapp' leftIcon={<WhatsappIcon/>} >
+                  Whatsapp
+                </Button>              
               </Box>
               <Box color='purple.600'>
                 <Heading as='h4'>#ParenDeMatarnos</Heading>
